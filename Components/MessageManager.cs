25c09814722cea1b27635f2a1204513f914ae8c7
Chris Hammond <github@christoc.com>
2016-01-31 21:46:44 -0600

    First commit

---
+++ Components/MessageManager.cs
@@ -0,0 +1,97 @@
+﻿/*
+' Copyright (c) 2016 Christoc.com
+'  All rights reserved.
+'
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+' documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+' the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+' to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System.Collections.Generic;
+using DotNetNuke.Data;
+using DotNetNuke.Framework;
+using Christoc.Modules.MessageOfTheDay.Models;
+
+namespace Christoc.Modules.MessageOfTheDay.Components
+{
+    interface IMessageManager
+    {
+        void CreateMessage(Message t);
+        void DeleteMessage(int MessageId, int moduleId);
+        void DeleteMessage(Message t);
+        IEnumerable<Message> GetMessages(int moduleId);
+        Message GetMessage(int MessageId, int moduleId);
+        void UpdateMessage(Message t);
+    }
+
+    class MessageManager : ServiceLocator<IMessageManager, MessageManager>, IMessageManager
+    {
+        public void CreateMessage(Message t)
+        {
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Message>();
+                rep.Insert(t);
+            }
+        }
+
+        public void DeleteMessage(int MessageId, int moduleId)
+        {
+            var t = GetMessage(MessageId, moduleId);
+            DeleteMessage(t);
+        }
+
+        public void DeleteMessage(Message t)
+        {
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Message>();
+                rep.Delete(t);
+            }
+        }
+
+        public IEnumerable<Message> GetMessages(int moduleId)
+        {
+            IEnumerable<Message> t;
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Message>();
+                t = rep.Get(moduleId);
+            }
+            return t;
+        }
+
+        public Message GetMessage(int MessageId, int moduleId)
+        {
+            Message t;
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Message>();
+                t = rep.GetById(MessageId, moduleId);
+            }
+            return t;
+        }
+
+        public void UpdateMessage(Message t)
+        {
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Message>();
+                rep.Update(t);
+            }
+        }
+
+        protected override System.Func<IMessageManager> GetFactory()
+        {
+            return () => new MessageManager();
+        }
+    }
+}
---
+++ Controllers/MessageController.cs
@@ -0,0 +1,92 @@
+﻿/*
+' Copyright (c) 2016 Christoc.com
+'  All rights reserved.
+'
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+' documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+' the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+' to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using Christoc.Modules.MessageOfTheDay.Components;
+using Christoc.Modules.MessageOfTheDay.Models;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Framework.JavaScriptLibraries;
+
+namespace Christoc.Modules.MessageOfTheDay.Controllers
+{
+    [DnnHandleError]
+    public class MessageController : DnnController
+    {
+
+        public ActionResult Delete(int MessageId)
+        {
+            MessageManager.Instance.DeleteMessage(MessageId, ModuleContext.ModuleId);
+            return RedirectToDefaultRoute();
+        }
+
+        public ActionResult Edit(int MessageId = -1)
+        {
+            DotNetNuke.Framework.JavaScriptLibraries.JavaScript.RequestRegistration(CommonJs.DnnPlugins);
+
+            var userlist = UserController.GetUsers(PortalSettings.PortalId);
+            var users = from user in userlist.Cast<UserInfo>().ToList()
+                        select new SelectListItem { Text = user.DisplayName, Value = user.UserID.ToString() };
+
+            ViewBag.Users = users;
+
+            var item = (MessageId == -1)
+                 ? new Message { ModuleId = ModuleContext.ModuleId }
+                 : MessageManager.Instance.GetMessage(MessageId, ModuleContext.ModuleId);
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [DotNetNuke.Web.Mvc.Framework.ActionFilters.ValidateAntiForgeryToken]
+        public ActionResult Edit(Message item)
+        {
+            if (item.MessageId == -1)
+            {
+                item.CreatedByUserId = User.UserID;
+                item.CreatedOnDate = DateTime.UtcNow;
+                item.LastModifiedByUserId = User.UserID;
+                item.LastModifiedOnDate = DateTime.UtcNow;
+
+                MessageManager.Instance.CreateMessage(item);
+            }
+            else
+            {
+                var existingItem = MessageManager.Instance.GetMessage(item.MessageId, item.ModuleId);
+                existingItem.LastModifiedByUserId = User.UserID;
+                existingItem.LastModifiedOnDate = DateTime.UtcNow;
+                existingItem.MessageTitle = item.MessageTitle;
+                existingItem.MessageDescription = item.MessageDescription;
+                
+
+                MessageManager.Instance.UpdateMessage(existingItem);
+            }
+
+            return RedirectToDefaultRoute();
+        }
+
+        [ModuleAction(ControlKey = "Edit", TitleKey = "AddMessage")]
+        public ActionResult Index()
+        {
+            var messages = MessageManager.Instance.GetMessages(ModuleContext.ModuleId);
+            return View(messages);
+        }
+    }
+}
